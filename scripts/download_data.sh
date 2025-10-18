5ef8215490d0f4b0ad26aaef73d2a82f7daf5ee5
Bazylevych Sofiia <bazylevych.pn@ucu.edu.ua>
2025-10-18 16:52:25 +0300

    update download_data.sh to download 2d_mri

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
+echo "Downloading datasets..."
 mkdir -p data/raw/mri_alz
+mkdir -p data/raw/2d_mri
 mkdir -p data/raw/oasis_2d_optional
 
+echo "-> jboysen/mri-and-alzheimers (metadata)"
 kaggle datasets download -d jboysen/mri-and-alzheimers -p data/raw/mri_alz --unzip
 
-#  для зовнішнього тесту на потім
-# kaggle datasets download -d <owner>/<alzheimer-mri-4classes> -p data/raw/oasis_2d_optional --unzip
+echo "-> marcopinamonti/alzheimer-mri-4-classes-dataset (MRI images)"
+kaggle datasets download -d marcopinamonti/alzheimer-mri-4-classes-dataset -p data/raw/2d_mri --unzip
 
-echo "Done. Raw data in data/raw/"
+echo "Done. Files saved in data/raw/"
